2a38be86c416bbefe22c7c1895aeec276b15783e
misike12 <59843249+misike12@users.noreply.github.com>
2024-03-18 20:23:24 +0100

    Delete shit

--- Backup/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Security;
-using System.Security.Permissions;
-
-[assembly: SecurityRules(SecurityRuleSet.Level1)]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
